cc795e933d64d9dd60e6eb932316f6841693c809
regoo707 <regoo707@gmail.com>
2019-10-21 19:24:55 +0200

    Add cors

--- Controllers/postsController.js
+++ Controllers/postsController.js
@@ -1,7 +1,6 @@
 (function(postsController){
     postsController.init = function(app){
-        var cors = require('cors')
-       
+        var cors = require('cors')      
           app.use(cors())
 
         app.get('/api/posts',cors(),function(req,res){
@@ -10,7 +9,7 @@
             res.send(posts);
         })
 
-        app.get('/api/posts/:id',function(req,res){
+        app.get('/api/posts/:id',cors(),function(req,res){
             var id = req.params.id;
             var posts = require('../Data/Posts');
             var post = posts.find(p=> p.id == id);
@@ -18,7 +17,7 @@
 
         })
 
-        app.get('/api/posts/userPosts/:id',function(req,res){
+        app.get('/api/posts/userPosts/:id',cors(),function(req,res){
             var id = req.params.id;
             var posts = require('../Data/Posts.json');
             var userPosts = posts.filter(p=> p.userId == id);
