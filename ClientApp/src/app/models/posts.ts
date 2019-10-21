c2f48490f1cd33e3c837affc9193c4cc0ac0d431
regoo707 <regoo707@gmail.com>
2019-10-21 19:24:55 +0200

    Add comments

--- ClientApp/src/app/models/posts.ts
+++ ClientApp/src/app/models/posts.ts
@@ -1,6 +1,9 @@
+import { comments } from './comments';
+
 export class posts{
     userId : number;
     id : number;
     title : string;
     body : string;
+    comments : comments[];
 }
--- Controllers/postsController.js
+++ Controllers/postsController.js
@@ -1,7 +1,6 @@
 (function(postsController){
     postsController.init = function(app){
-        var cors = require('cors')
-       
+        var cors = require('cors')      
           app.use(cors())
 
         app.get('/api/posts',cors(),function(req,res){
@@ -10,19 +9,29 @@
             res.send(posts);
         })
 
-        app.get('/api/posts/:id',function(req,res){
+        app.get('/api/posts/:id',cors(),function(req,res){
             var id = req.params.id;
             var posts = require('../Data/Posts');
+            var comments = require('../Data/comments')
             var post = posts.find(p=> p.id == id);
+            post.comments =  comments.filter(c=> c.postId == id);
             res.send(post);
 
         })
 
-        app.get('/api/posts/userPosts/:id',function(req,res){
+        app.get('/api/posts/userPosts/:id',cors(),function(req,res){
             var id = req.params.id;
             var posts = require('../Data/Posts.json');
             var userPosts = posts.filter(p=> p.userId == id);
             res.send(userPosts);
+        })
+
+        app.get('/api/comments/:id',cors(),function(req,res){
+            var id = req.params.id;
+            var comments = require('../Data/comments')
+            var postcomments = comments.filter(c=> c.postId == id);
+            res.set('Content_Type','application/json');
+            res.send(postcomments);
         })
     }   
 })(module.exports)
